183adea58541022cf52aed9e1c1dc781775a781c
ABRARgomaa <abrargomaa111@gmail.com>
2023-08-22 07:55:07 -0700

    h

---
+++ main.h
@@ -0,0 +1,66 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+#define BF_SIZE 1024
+#define BF_FLSH -1
+#define NLL_STR "(null)"
+#define PRMS_I {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+/**
+* struct paramets - good
+* @us: good
+* @pls_f: good
+* @spc_f: good
+* @hsh_f: good
+* @z_f: good
+* @min_f: good
+* @wdth: good
+* @prcs: good
+* @h_mod: good
+* @l_mod: good
+*/
+/**
+* struct sp - good
+* @sp: good
+* @f: good
+*/
+typedef struct paramets
+{
+unsigned int us : 1;
+unsigned int pls_f : 1;
+unsigned int spc_f : 1;
+unsigned int hsh_f : 1;
+unsigned int z_f : 1;
+unsigned int min_f : 1;
+unsigned int wdth : 1;
+unsigned int prcs : 1;
+unsigned int h_mod : 1;
+unsigned int l_mod : 1;
+} prm_t;
+typedef struct sp
+{
+char *sp;
+int (*f)(va_list, prm_t *);
+} spcfr_t;
+int _pt(char *string);
+int _ptchar(int ch);
+int prnt_chr(va_list a, prm_t *prm);
+int prnt_int(va_list a, prm_t *prm);
+int prnt_strng(va_list a, prm_t *prm);
+int prnt_prcnt(va_list a, prm_t *prm);
+int prnt_sc(va_list a, prm_t *prm);
+int (*gt_spcfr(char *str))(va_list a, prm_t *prm);
+int gt_prnt(char *str, va_list a, prm_t *prm);
+int gt_flag(char *str, prm_t *prm);
+int gt_mod(char *str, prm_t *prm);
+char *gt_wdth(char *str, prm_t *prm, va_list a);
+int prnt_frm_t(char *strt, char *stp, char *excpt);
+int prnt_rv(va_list a, prm_t *prm);
+int prnt_rt3(va_list a, prm_t *prm);
+void prms(prm_t *prm, va_list a);
+char *gt_prcs(char *pr, prm_t *prm, va_list a);
+int _printf(const char *format, ...);
+#endif
